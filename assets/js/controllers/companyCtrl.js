ab4cacae6f36b715a69fee137c7d251f3249e6ca
waqasjamal <waqasjamal.wfh@gmail.com>
2017-03-17 19:23:32 +0500

    company edit work

--- assets/js/controllers/companyCtrl.js
+++ assets/js/controllers/companyCtrl.js
@@ -323,7 +323,7 @@ app.controller('ModalUiCtrl', ["$scope", "$rootScope", "$uibModalInstance", "ite
           title: 'Successful',
           text: 'Company Edit Successfully'
         };
-        return toaster.pop($scope.toaster.type, $scope.toaster.title,$scope.toaster.text);
+         toaster.pop($scope.toaster.type, $scope.toaster.title,$scope.toaster.text);
          $uibModalInstance.dismiss('cancel');
         
         $state.go('app.listcompany', {}, { reload: true })
