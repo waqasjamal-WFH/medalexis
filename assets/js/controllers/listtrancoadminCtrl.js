958d726f6598d20cca42899fddfc146eb3df48f0
waqasjamal <waqasjamal.wfh@gmail.com>
2017-03-21 14:55:45 +0500

    loader for list company

--- assets/js/controllers/listtrancoadminCtrl.js
+++ assets/js/controllers/listtrancoadminCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 // .......add company controller start ..............................///
-app.controller('addadminCtrl', ["$scope","$location",function($scope,$location){
+app.controller('addadminCtrl', ["$scope","$location","$http","$localStorage",function($scope,$location,$http,$localStorage){
     //................................http post request for getting company list start here........................
         // var datas=[];
         var param={'token' :$localStorage.user_data.response.token};
