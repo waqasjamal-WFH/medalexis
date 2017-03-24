fecde89218e4b510dfee41efd5645d71442e0c2b
waqasjamal <waqasjamal.wfh@gmail.com>
2017-03-24 19:44:23 +0500

    waqas

--- assets/js/controllers/listtrancoadminCtrl.js
+++ assets/js/controllers/listtrancoadminCtrl.js
@@ -339,10 +339,10 @@ app.controller('listadminCtrl', ["$scope", "$filter", "ngTableParams","$uibModal
            
             var datas=response.data.data;
             // console.log(datas);
-            datas.forEach(function(data){
-                console.log(data.companies);
-                var com= data.companies.replace(/>>/g, " , ");
-                data.companies=com;
+            datas.forEach(function(datas){
+                console.log(datas.companies);
+                // var com= data.companies.replace(/>>/g, " , ");
+                // data.companies=com;
             });
             console.log(datas);
 
