0a85afb49e1bc79311d1cc320f49b717da0fe7cd
waqasjamal <waqasjamal.wfh@gmail.com>
2017-03-29 15:37:18 +0500

    waqas

--- assets/js/controllers/listtrancoadminCtrl.js
+++ assets/js/controllers/listtrancoadminCtrl.js
@@ -434,10 +434,10 @@ app.controller('listadminCtrl', ["$scope", "$filter", "ngTableParams","$uibModal
 
 
     // ...............model open function for assigning qa and transcriber start here..............//
-    $scope.openmodel = function () {
+    $scope.openmodel = function (uid) {
       // $scope.taskid=taskId
-      // console.log(taskId);
-    
+      console.log(uid);
+     $scope.userid= uid;
       var modalInstance = $uibModal.open({
 
         templateUrl: 'myModalContent1.html',
@@ -447,7 +447,7 @@ app.controller('listadminCtrl', ["$scope", "$filter", "ngTableParams","$uibModal
         backdrop: 'static',
         resolve: {
           items: function () {
-            // return $scope.taskid;
+            return $scope.userid;
             // $scope.list=qatranlist;
             // console.log(qatranlist);
           }
@@ -459,6 +459,8 @@ app.controller('listadminCtrl', ["$scope", "$filter", "ngTableParams","$uibModal
       }, function () {
         $log.info('Modal dismissed at: ' + new Date());
       });
+      $scope.showLoader = true;
+      $scope.showform = false;
     };
 
     // ...............model open function for assigning qa and transcriber end here..............//
@@ -474,125 +476,170 @@ app.controller('listadminCtrl', ["$scope", "$filter", "ngTableParams","$uibModal
 app.controller('ModalUiCtrl', ["$scope", "$rootScope", "$uibModalInstance", "items","$http","$location","PDFKit","$sce","$localStorage", function ($scope, $rootScope, $uibModalInstance, items,$http, $location,PDFKit,$sce,$localStorage) {
   
     $scope.access_right=[
-    "Add Company",
-    "List Company",
-    "Add Tranco Admin",
-    "List Tranco Admin",
-    "Add Transcriber",
-    "List Transcriber",
-    "Add QA",
-    "List QA",
-    "Add Doctor",
-    "List Doctor",
-    "Add Nurse",
-    "List Nurse",
-    "Add Practice Admin",
-    "List Practice Admin",
-    "Add Appoinment",
-    "List Appoinment",
-    "Add Receptionist",
-    "List Receptionist",
-    "Add Patient",
-    "List Patient"
+        {
+            "name": "Add Company",
+            "status": 1,
+            "column_name":"add_company",
+            "parent_column_name": "company"
+        },{
+            "name": "List Company",
+            "status": 1,
+            "column_name":"list_company",
+            "parent_column_name": "company"
+        },{
+            "name": "Add Tranco Admin",
+            "status": 1,
+            "column_name":"add_trancoadmin",
+            "parent_column_name": "tranco_admin"
+        },{
+            "name": "List Tranco Admin",
+            "status": 1,
+            "column_name":"list_trancoadmin",
+            "parent_column_name": "tranco_admin"
+        },{
+            "name": "Add Transcriber",
+            "status": 1,
+            "column_name":"add_transcriber",
+            "parent_column_name": "transcriber"
+        },{
+            "name": "List Transcriber",
+            "status": 1,
+            "column_name":"list_transcriber",
+            "parent_column_name": "transcriber"
+        },{
+            "name": "Add QA",
+            "status": 1,
+            "column_name":"add_QA",
+            "parent_column_name": "quality_assurance"
+        },{
+            "name": "List QA",
+            "status": 1,
+            "column_name":"list_QA",
+            "parent_column_name": "quality_assurance"
+        },{
+            "name": "Add Doctor",
+            "status": 1,
+            "column_name":"add_doctor",
+            "parent_column_name": "doctor"
+        },{
+            "name": "List Doctor",
+            "status": 1,
+            "column_name":"list_doctor",
+            "parent_column_name": "doctor"
+        },{
+            "name": "Add Nurse",
+            "status": 1,
+            "column_name":"add_nurse",
+            "parent_column_name": "nurse"
+        },{
+            "name": "List Nurse",
+            "status": 1,
+            "column_name":"list_nurse",
+            "parent_column_name": "nurse"
+        },{
+            "name": "Add Practice Admin",
+            "status": 1,
+            "column_name":"add_practiceadmin",
+            "parent_column_name": "practice_admin"
+        },{
+            "name": "List Practice Admin",
+            "status": 1,
+            "column_name":"list_practiceadmin",
+            "parent_column_name": "practice_admin"
+        },{
+            "name": "Add Appoinment",
+            "status": 1,
+            "column_name":"add_appoinment",
+            "parent_column_name": "appoinment"
+        },{
+            "name": "List Appoinment",
+            "status": 1,
+            "column_name":"list_appoinment",
+            "parent_column_name": "appoinment"
+        },{
+            "name": "Add Receptionist",
+            "status": 1,
+            "column_name":"add_receptionist",
+            "parent_column_name": "receptionist"
+        },{
+            "name": "List Receptionist",
+            "status": 1,
+            "column_name":"list_receptionist",
+            "parent_column_name": "receptionist"
+        },{
+            "name": "Add Patient",
+            "status": 1,
+            "column_name":"add_patient",
+            "parent_column_name": "patient"
+        },{
+            "name": "List Patient",
+            "status": 1,
+            "column_name":"list_patient",
+            "parent_column_name": "patient"
+        }
     ];
 
-    // $scope.companies=[
-    // "Company 1",
-    // "Company 2",
-    // "Company 3",
-    // "Company 4",
-    // "Company 5",
-    // "Company 6",
-    // "Company 7",
-    // "Company 8",
-    // "Company 9",
-    // "Company 10",
-    // "Company 11",
-    // "Company 12",
-    
-    // "Company 13"
-    // ];
-
     //................................http post request for getting company list start here........................
-        // var datas=[];
-        var param={'token' :$localStorage.user_data.response.token};
-        $http.post($location.protocol()+"://"+$location.host()+"/medilixis_server/public/getCompany", param)
-        .then(function(response) {
-          // console.log(response.data);
-          if(response.data.status=="success"){
-           
-            $scope.companies=response.data.data;
-            // console.log($scope.companies);
-            
-          }else{
-           
-          } 
-        }).catch(function(){
-              console.log("error adding company");
-        }); 
-      //.............................http post request for getting company list end here...............................
+    // var datas=[];
+    var param={'token' :$localStorage.user_data.response.token};
+    $http.post($location.protocol()+"://"+$location.host()+"/medilixis_server/public/getCompany", param)
+    .then(function(response) {
+      // console.log(response.data);
+      if(response.data.status=="success"){
+       
+        $scope.companies=response.data.data;
+        // console.log($scope.companies);
+        
+      }else{
+       
+      } 
+    }).catch(function(){
+          console.log("error adding company");
+    }); 
+  //.............................http post request for getting company list end here...............................
 
-    // $scope.selectOptionsObjects = [
-    //     {
-    //         id: 0,
-    //         name: "Apples"
-    //     },
-    //     {
-    //         id: 1,
-    //         name: "Bananas"
-    //     },
-    //     {
-    //         id: 2,
-    //         name: "Peaches"
-    //     }
-    // ];
+//
 
-
-
-
-
- // ...............http call for list of transcriber and qa start here....................//
-    // var qatranlist;
-  //   $http.post($location.protocol()+"://"+$location.host()+"/muapp-new/muapp/qatranlist")
-  //   .then(function(response,data) {
-
-  //     //console.log(response);
-  //     if(response.data.result=="success"){
-  //       console.log(response.data.data);
-  //       $scope.list=response.data.data
-  //        // qatranlist=response.data.data
-  //     }else{
-  //      $scope.list=response.data.data
-  //     }
-  //   }).catch(function(){
-  //   // console.log(userdata);
-  // });  
-
-    // ...............http call for list of transcriber and qa end here....................//
   $scope.items = items;
   $scope.selected = {
     item: $scope.list
   };
-//.........................get selected transcriber and qa for a selected task START here.....................//
-   // $http.get(
-   //  $location.protocol()+"://"+$location.host()+"/muapp-new/muapp/gettask",
-   //  {params:{"task_ID":$scope.taskid}})
-   //  .then(function(response,data) {
-   //    console.log(response.data);
-   //    $scope.assignedQA=response.data.result[1].username;
-   //    $scope.assignedtrans=response.data.result[0].username;
 
-   //    $scope.assignedQAID=response.data.result[1].id;
-   //    $scope.assignedtransID=response.data.result[0].id;
-   //    console.log( "hello "+$scope.assignedQAID);
-   //    console.log( "hello "+$scope.assignedtransID);
-   //    $scope.qa=response.data.result[1].id;
-   //    $scope.transcriber=response.data.result[0].id;
-   //  })
+  //................................http call for get selected user data start here..................................
+  var param={
+            'token' :$localStorage.user_data.response.token,
+            'uid': $scope.userid
+    };
 
-  //.........................get selected transcriber and qa for a selected task END here.....................//
-  
-  
+    $http.post($location.protocol()+"://"+$location.host()+"/medilixis_server/public/getselectedtrancoadmin", param)
+    .then(function(response) {
+      if(response.data.status=="success"){
+        $scope.showLoader = false;
+        $scope.showform = true;
+        console.log(response.data);
+        
+        // $scope.onecompany=response.data.data;
+      
+        // $scope.short_name=$scope.onecompany[0].short_name;
+        // $scope.full_name=$scope.onecompany[0].full_name;
+        // $scope.address=$scope.onecompany[0].address;
+        // // $scope.date=$scope.onecompany[0].date;
+        // $scope.city=$scope.onecompany[0].city;
+        // $scope.state=$scope.onecompany[0].state;
+        // $scope.zip_code=$scope.onecompany[0].zip_code;
+        // $scope.country=$scope.onecompany[0].country;
+        // $scope.phone=$scope.onecompany[0].phone;
+        // $scope.fax=$scope.onecompany[0].fax;
+        // $scope.e_mail=$scope.onecompany[0].e_mail;
+        // $scope.website=$scope.onecompany[0].web_address;
+        // $scope.timezone=$scope.onecompany[0].time_zone;
+        // $scope.admin_person_name=$scope.onecompany[0].admin_person_name;
+      }else{
+              
+      } 
+
+
+  //................................http call for get selected user data start here..................................
 
   //....................on click ok button on assigning qa and transcriber model id inserted to mysql table task_ permission START....////
   $scope.ok = function () {
