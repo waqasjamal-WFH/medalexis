9af67e0547de1e8b7158e214a11d666cd9ec3eb0
waqasjamal <waqasjamal.wfh@gmail.com>
2017-03-30 20:00:58 +0500

    waqas

--- assets/js/controllers/listtrancoadminCtrl.js
+++ assets/js/controllers/listtrancoadminCtrl.js
@@ -670,7 +670,7 @@ app.controller('ModalUiCtrl', ["$scope", "$rootScope", "$uibModalInstance", "ite
             
             if(valuess.company_short_name){
                 var all_companies= $scope.companies;
-                // console.log(all_companies);
+                console.log(all_companies);
                 angular.forEach(all_companies, function(valu, ke) {
                     // console.log(valu.id);
                     // console.log(valuess.company_id);
