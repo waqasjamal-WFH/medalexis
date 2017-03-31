58c2ffb713bd2d7d61bc336b2f5148bb9c45eb34
waqasjamal <waqasjamal.wfh@gmail.com>
2017-03-31 14:37:41 +0500

    waqas

--- assets/js/controllers/listtrancoadminCtrl.js
+++ assets/js/controllers/listtrancoadminCtrl.js
@@ -691,7 +691,7 @@ app.controller('ModalUiCtrl', ["$scope", "$rootScope", "$uibModalInstance", "ite
       }else{
               
       } 
-    }) }, 1000);
+    }) }, 2000);
 
   //................................http call for get selected user data start here..................................
 
