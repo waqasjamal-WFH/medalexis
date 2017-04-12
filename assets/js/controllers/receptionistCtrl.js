d358e72d5a797bed092faa848ebfbf01e3610b5c
waqasjamal <waqasjamal.wfh@gmail.com>
2017-04-12 14:37:11 +0500

    waqas

--- assets/js/controllers/receptionistCtrl.js
+++ assets/js/controllers/receptionistCtrl.js
@@ -1,62 +1,182 @@
 'use strict';
 // .......add company controller start ..............................///
 app.controller('addreceptionistCtrl', ["$scope","$location",function($scope,$location){
-	$scope.name="hello practice admin";
+	 //................................http post request for getting company list start here........................
+        // var datas=[];
+        var param={'token' :$localStorage.user_data.response.token};
+        $http.post($location.protocol()+"://"+$location.host()+"/medilixis_server/public/getCompany", param)
+        .then(function(response) {
+          // console.log(response.data);
+          if(response.data.status=="success"){
+           
+            $scope.companies=response.data.data;
+            console.log($scope.companies);
+            
+          }else{
+           
+          } 
+        }).catch(function(){
+              console.log("error adding company");
+        }); 
+    //.............................http post request for getting company list end here...............................
     $scope.access_right=[
-    "Add Company",
-    "List Company",
-    "Add Tranco Admin",
-    "List Tranco Admin",
-    "Add Transcriber",
-    "List Transcriber",
-    "Add QA",
-    "List QA",
-    "Add Doctor",
-    "List Doctor",
-    "Add Nurse",
-    "List Nurse",
-    "Add Practice Admin",
-    "List Practice Admin",
-    "Add Appoinment",
-    "List Appoinment",
-    "Add Receptionist",
-    "List Receptionist",
-    "Add Patient",
-    "List Patient"
-    ];
-
-    $scope.companies=[
-    "Company 1",
-    "Company 2",
-    "Company 3",
-    "Company 4",
-    "Company 5",
-    "Company 6",
-    "Company 7",
-    "Company 8",
-    "Company 9",
-    "Company 10",
-    "Company 11",
-    "Company 12",
-    
-    "Company 13"
-    ];
-
-
-    $scope.selectOptionsObjects = [
         {
-            id: 0,
-            name: "Apples"
-        },
-        {
-            id: 1,
-            name: "Bananas"
-        },
-        {
-            id: 2,
-            name: "Peaches"
+            "name": "Add Company",
+            "status": 1,
+            "column_name":"add_company",
+            "parent_column_name": "company"
+        },{
+            "name": "List Company",
+            "status": 1,
+            "column_name":"list_company",
+            "parent_column_name": "company"
+        },{
+            "name": "Add Tranco Admin",
+            "status": 1,
+            "column_name":"add_trancoadmin",
+            "parent_column_name": "tranco_admin"
+        },{
+            "name": "List Tranco Admin",
+            "status": 1,
+            "column_name":"list_trancoadmin",
+            "parent_column_name": "tranco_admin"
+        },{
+            "name": "Add Transcriber",
+            "status": 1,
+            "column_name":"add_transcriber",
+            "parent_column_name": "transcriber"
+        },{
+            "name": "List Transcriber",
+            "status": 1,
+            "column_name":"list_transcriber",
+            "parent_column_name": "transcriber"
+        },{
+            "name": "Add QA",
+            "status": 1,
+            "column_name":"add_QA",
+            "parent_column_name": "quality_assurance"
+        },{
+            "name": "List QA",
+            "status": 1,
+            "column_name":"list_QA",
+            "parent_column_name": "quality_assurance"
+        },{
+            "name": "Add Doctor",
+            "status": 1,
+            "column_name":"add_doctor",
+            "parent_column_name": "doctor"
+        },{
+            "name": "List Doctor",
+            "status": 1,
+            "column_name":"list_doctor",
+            "parent_column_name": "doctor"
+        },{
+            "name": "Add Nurse",
+            "status": 1,
+            "column_name":"add_nurse",
+            "parent_column_name": "nurse"
+        },{
+            "name": "List Nurse",
+            "status": 1,
+            "column_name":"list_nurse",
+            "parent_column_name": "nurse"
+        },{
+            "name": "Add Practice Admin",
+            "status": 1,
+            "column_name":"add_practiceadmin",
+            "parent_column_name": "practice_admin"
+        },{
+            "name": "List Practice Admin",
+            "status": 1,
+            "column_name":"list_practiceadmin",
+            "parent_column_name": "practice_admin"
+        },{
+            "name": "Add Appoinment",
+            "status": 1,
+            "column_name":"add_appoinment",
+            "parent_column_name": "appoinment"
+        },{
+            "name": "List Appoinment",
+            "status": 1,
+            "column_name":"list_appoinment",
+            "parent_column_name": "appoinment"
+        },{
+            "name": "Add Receptionist",
+            "status": 1,
+            "column_name":"add_receptionist",
+            "parent_column_name": "receptionist"
+        },{
+            "name": "List Receptionist",
+            "status": 1,
+            "column_name":"list_receptionist",
+            "parent_column_name": "receptionist"
+        },{
+            "name": "Add Patient",
+            "status": 1,
+            "column_name":"add_patient",
+            "parent_column_name": "patient"
+        },{
+            "name": "List Patient",
+            "status": 1,
+            "column_name":"list_patient",
+            "parent_column_name": "patient"
         }
     ];
+
+
+     $scope.submit= function(){
+        var first_name=$scope.first_name;
+        var last_name=$scope.last_name;
+        var email=$scope.email;
+        var password=$scope.password;
+        var address=$scope.address;
+        var phone_number=$scope.phone_number;
+        var access_rights=$scope.access_rights;
+        var associate_company=$scope.associate_company;
+        var state=$scope.state;
+        var country=$scope.country;
+        var city=$scope.city;
+        var token=$localStorage.user_data.response.token;
+
+        var param= {"token":token ,
+          "username":first_name,
+          "last_name":last_name,
+          "email":email,
+          "password":password,
+          "address":address,
+          "phone_number":phone_number,
+          "access_rights":access_rights,
+          "associate_company":associate_company,
+          "state":state,
+          "country":country,
+          "city":city,
+          "role_id":8
+        };
+
+
+        $http.post($location.protocol()+"://"+$location.host()+"/medilixis_server/public/addreceptionist", param)
+        .then(function(response) {
+          // console.log(response.data);
+           if(response.data.status=="success"){
+                $scope.toaster = {
+                  type: 'success',
+                  title: 'Successful',
+                  text: 'Receptionist Added Successfully'
+                };
+                $location.path('app/listreceptionist');
+                return toaster.pop($scope.toaster.type, $scope.toaster.title,$scope.toaster.text);
+            }else{
+                $scope.toaster = {
+                  type: 'error',
+                  title: 'Unsuccessful',
+                  text: 'Error adding Receptionist'
+                };
+                return toaster.pop($scope.toaster.type, $scope.toaster.title,$scope.toaster.text);
+            }
+        }).catch(function(){
+              console.log("Error adding Receptionist");
+        }); 
+    }
 }]);
 
 // .......add company controller end ..............................///
