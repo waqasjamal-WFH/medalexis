0315e3edfd84fe2867be623c0220c994da581c34
waqasjamal <waqasjamal.wfh@gmail.com>
2017-04-17 15:34:14 +0500

    waqas

--- assets/js/controllers/patientCtrl.js
+++ assets/js/controllers/patientCtrl.js
@@ -250,8 +250,8 @@ app.controller('ModalUiCtrlpatient', ["$scope", "$rootScope", "$uibModalInstance
         $scope.first_name=response.data.data[0].first_name;
         $scope.last_name=response.data.data[0].last_name;
         
-        $scope.dob=new Date(response.data.data[0].date_of_birth);
-        $scope.addresss=response.data.data[0].address;
+        $scope.do_b=new Date(response.data.data[0].date_of_birth);
+        $scope.ad_dresss=response.data.data[0].address;
         $scope.phone__number=response.data.data[0].phone;
         $scope.ci_ty=response.data.data[0].city;
         $scope.st_ate=response.data.data[0].state;
